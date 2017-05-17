7cb5fa89476bd574b3e97183c5512c564f41ba65
rifaie <rifaie@192.168.1.5>
2017-05-17 09:44:33 +0530

    Base Member panal

--- web/js/vaydeal.jquery.js
+++ web/js/vaydeal.jquery.js
@@ -1,206 +1,206 @@
 /* jQuery INFORMATION
- - Project : seller vdeal
- - Version : v1.0.0
- - Last Update : 14 January 2017
- - Copyright 2016-2017
- */
+   - Project : seller vdeal
+   - Version : v1.0.0
+   - Last Update : 14 January 2017
+   - Copyright 2016-2017
+*/
 
-$(document).ready(function () {
+$(document).ready(function() {
 
-    // Modal
-    $('.modal').modal();
+	// Modal
+	$('.modal').modal();
 
-    // Select Option
-    $('select').material_select();
+	// Select Option
+	$('select').material_select();
 
-    // Tooltip
-    $('.tooltipped').tooltip({delay: 50});
-
+	// Tooltip
+	$('.tooltipped').tooltip({delay: 50});
+    
     // Tabs
     $('div.tabs').tabs();
+    
+	// Auto Fill 
+	$("[name='countries']").autocomplete({ // Countries
+	    data: {
+	        "Argentina": null,
+			"Bahrain": null,
+			"Canada": null,
+			"Denmark": null,
+			"Ecuador": null,
+			"France": null,
+			"Germany": null,
+			"Honduras": null,
+			"India ": null,
+			"Japan": null
+	    },
+	    limit: 10,
+	});
 
-    // Auto Fill 
-    $("[name='countries']").autocomplete({// Countries
-        data: {
-            "Argentina": null,
-            "Bahrain": null,
-            "Canada": null,
-            "Denmark": null,
-            "Ecuador": null,
-            "France": null,
-            "Germany": null,
-            "Honduras": null,
-            "India ": null,
-            "Japan": null
-        },
-        limit: 10,
-    });
+	$("[name='state']").autocomplete({ // State
+	    data: {
+	        "Arunachal Pradesh": null,
+			"Assam": null,
+			"Bihar": null,
+			"Chhattisgarh": null,
+			"Jharkhand": null,
+			"Karnataka": null,
+			"Kerala": null,
+			"Madhya Pradesh": null,
+			"Maharashtra": null
+	    },
+	    limit: 10,
+	});
 
-    $("[name='state']").autocomplete({// State
-        data: {
-            "Arunachal Pradesh": null,
-            "Assam": null,
-            "Bihar": null,
-            "Chhattisgarh": null,
-            "Jharkhand": null,
-            "Karnataka": null,
-            "Kerala": null,
-            "Madhya Pradesh": null,
-            "Maharashtra": null
-        },
-        limit: 10,
-    });
-
-    $("[name='districts']").autocomplete({// Districts
-        data: {
-            "Alappuzha": null,
-            "Ernakulam": null,
-            "Idukki": null,
-            "Kannur": null,
-            "Kasaragod": null,
-            "Kollam": null,
-            "Kottayam": null,
-            "Kozhikode": null,
-            "Malappuram ": null,
-            "Palakkad": null,
-            "Pathanamthitta": null,
-            "Thiruvananthapuram": null,
-            "Thrissur": null,
-            "Wayanad": null
-        },
-        limit: 10,
-    });
+	$("[name='districts']").autocomplete({ // Districts
+	    data: {
+	        "Alappuzha": null,
+			"Ernakulam": null,
+			"Idukki": null,
+			"Kannur": null,
+			"Kasaragod": null,
+			"Kollam": null,
+			"Kottayam": null,
+			"Kozhikode": null,
+			"Malappuram ": null,
+			"Palakkad": null,
+			"Pathanamthitta": null,
+			"Thiruvananthapuram": null,
+			"Thrissur": null,
+			"Wayanad": null
+	    },
+	    limit: 10,
+	});
 
 });
 
 
 
 // login Submit
-function logSubmit() {
-    $.post("", {
-        name: $('#userName').val(),
-        password: $('#userPassword').val()
-    }, function (data) {
-        alert('Login')
-    });
+function logSubmit(){
+	$.post("", {
+		name: $('#userName').val(),
+		password: $('#userPassword').val()
+	}, function(data){
+		alert('Login')
+	});
 }
 
 // Forgot Password Input
-function fogtPassword() {
-    $(function (data) {
-        alert('Forgot your password');
-    });
+function fogtPassword(){
+	$(function (data){
+		alert('Forgot your password');
+	});
 }
 
 // Add Location Submit
-function addLocation() {
-    $.post("", {
+function addLocation(){
+    $.post("",{
         rolnumber: $('#rolNum').val(),
-        countries: $('#adCou').val(),
-        state: $('#adSta').val(),
-        districts: $('#adDis').val(),
-        location: $('#adLoc').val()
-    }, function (data) {
-        alert('Add Location')
-    })
+		countries: $('#adCou').val(),
+		state: $('#adSta').val(),
+		districts: $('#adDis').val(),
+		location: $('#adLoc').val()
+    }, function(data){
+		alert('Add Location')
+	})
 }
 
 // Edit Location Submit
-function edtLocation() {
-    $.post("", {
+function edtLocation(){
+    $.post("",{
         rolnumber: $('#rolNum').val(),
-        countries: $('#edtNum').val(),
-        state: $('#edtSta').val(),
-        districts: $('#edtDis').val(),
-        location: $('#edtLoc').val()
-    }, function (data) {
-        alert('Edit and Update Location')
-    })
+		countries: $('#edtNum').val(),
+		state: $('#edtSta').val(),
+		districts: $('#edtDis').val(),
+		location: $('#edtLoc').val()
+    }, function(data){
+		alert('Edit and Update Location')
+	})
 }
 
 // Add Post Submit
-function addPost() {
-    $.post("", {
+function addPost(){
+    $.post("",{
         rolnumber: $('#rolNum').val(),
-        countries: $('#adCou').val(),
-        state: $('#adSta').val(),
-        districts: $('#adDis').val(),
-        subdistricts: $('#adSubdis').val(),
+		countries: $('#adCou').val(),
+		state: $('#adSta').val(),
+		districts: $('#adDis').val(),
+		subdistricts: $('#adSubdis').val(),
         village: $('#adVlg').val(),
         pin: $('#adPin').val(),
         post: $('#adPst').val()
-    }, function (data) {
-        alert('Add Post')
-    })
+    }, function(data){
+		alert('Add Post')
+	})
 }
 
 // Edit Post Submit
-function edtPost() {
-    $.post("", {
+function edtPost(){
+    $.post("",{
         rolnumber: $('#rolNum').val(),
-        countries: $('#edtCou').val(),
-        state: $('#edtSta').val(),
-        districts: $('#edtDis').val(),
-        subdistricts: $('#edtSubdis').val(),
+		countries: $('#edtCou').val(),
+		state: $('#edtSta').val(),
+		districts: $('#edtDis').val(),
+		subdistricts: $('#edtSubdis').val(),
         village: $('#edtVlg').val(),
         pin: $('#edtPin').val(),
         post: $('#edtPst').val()
-    }, function (data) {
-        alert('Edit and Update Post')
-    })
+    }, function(data){
+		alert('Edit and Update Post')
+	})
 }
 
 // Add Critevia
-function addCritevia() {
-    $.post("", {
+function addCritevia(){
+    $.post("",{
         rolnumber: $('#rolNum').val(),
         product: $('#adPro').val(),
         valtype: $('#adTyp').val()
-    }, function (data) {
+    }, function(data){
         alert('Add Critevia')
     })
 }
 
 // Edit Critevia
-function edtCritevia() {
-    $.post("", {
+function edtCritevia(){
+    $.post("",{
         rolnumber: $('#rolNum').val(),
         product: $('#edtPro').val(),
         valtype: $('#edtTyp').val()
-    }, function (data) {
+    }, function(data){
         alert('Edit And update Critevia')
     })
 }
 
 // Add CTV
-function addCtv() {
-    $.post("", {
+function addCtv(){
+    $.post("",{
         rolnumber: $('#rolNum').val(),
         ctvplan: $('#adCplan').val(),
         ctvamount: $('#adCamount').val(),
         ctvtown: $('#adCtown').val(),
         ctvdiscount: $('#adCdisc').val()
-    }, function (data) {
+    }, function(data){
         alert('Add New CTV')
     })
 }
 
 // Edit CTV
-function edtCtv() {
-    $.post("", {
+function edtCtv(){
+    $.post("",{
         rolnumber: $('#rolNum').val(),
         ctvplan: $('#edtCplan').val(),
         ctvamount: $('#edtCamount').val(),
         ctvtown: $('#edtCtown').val(),
         ctvdiscount: $('#edtCdisc').val()
-    }, function (data) {
+    }, function(data){
         alert('Edit and Update CTV')
     })
 }
 
 // Add Plan 
-function addPlan() {
-    $.post("", {
+function addPlan(){
+    $.post("",{
         rolnumber: $('#rolNum').val(),
         planname: $('#adName').val(),
         planamount: $('#adAmount').val(),
@@ -212,14 +212,14 @@ function addPlan() {
         townvisible: $('#adTv').val(),
         shoplisting: $('#adSlf').val(),
         coupenvisible: $('#adCv').val()
-    }, function (data) {
+    }, function(data){
         alert('Add New Plan')
     })
 }
 
 // Edit Plan 
-function edtPlan() {
-    $.post("", {
+function edtPlan(){
+    $.post("",{
         rolnumber: $('#rolNum').val(),
         planname: $('#edtName').val(),
         planamount: $('#edtAmount').val(),
@@ -231,24 +231,24 @@ function edtPlan() {
         townvisible: $('#edtTv').val(),
         shoplisting: $('#edtSlf').val(),
         coupenvisible: $('#edtCv').val()
-    }, function (data) {
+    }, function(data){
         alert('Edit and Update Plan')
     })
 }
 
 // Edit Plan Premium Customer
-function edtPc() {
-    $.post("", {
+function edtPc(){
+    $.post("",{
         rolnumber: $('#rolNum').val(),
         pcamount: $('#edtPcamount').val()
-    }, function (data) {
+    }, function(data){
         alert('Edit and Update Preemium Customer')
     })
 }
 
 // Add User
-function addUser() {
-    $.post("", {
+function addUser(){
+    $.post("",{
         rolnumber: $('#rolNum').val(),
         name: $('#adName').val(),
         address1: $('#adAdr1').val(),
@@ -257,14 +257,14 @@ function addUser() {
         pin: $('#adPin').val(),
         usertype: $('#adUtype').val(),
         email: $('#adEmail').val()
-    }, function (data) {
+    }, function(data){
         alert('Add New User')
     })
 }
 
 // Edit User
-function edtUser() {
-    $.post("", {
+function edtUser(){
+    $.post("",{
         rolnumber: $('#rolNum').val(),
         name: $('#edtName').val(),
         address1: $('#edtAdr1').val(),
@@ -273,62 +273,51 @@ function edtUser() {
         pin: $('#edtPin').val(),
         usertype: $('#edtUtype').val(),
         email: $('#edtEmail').val()
-    }, function (data) {
+    }, function(data){
         alert('Edit and update User')
     })
 }
 
 // Add User Type 
-function addUsertype() {
-    $.post("", {
+function addUsertype(){
+    $.post("",{
         rolnumber: $('#rolNum').val(),
         type: $('#adType').val(),
         supertype: $('#adSpt').val()
-    }, function (data) {
+    }, function(data){
         alert('Add New User Type')
     })
 }
 
 // Edit User Type 
-function edtUsertype() {
-    $.post("", {
+function edtUsertype(){
+    $.post("",{
         rolnumber: $('#rolNum').val(),
         type: $('#edtType').val(),
         supertype: $('#edtSpt').val()
-    }, function (data) {
+    }, function(data){
         alert('Edit and Update User Type')
     })
 }
 
 // Add Super Category 
-function addSc() {
-    $.post("", {
+function addSc(){
+    $.post("",{
         rolnumber: $('#rolNum').val(),
         categoryname: $('#adCn').val()
-    }, function (data) {
+    }, function(data){
         alert('Add New Super Category')
     })
 }
 
 // Edit Super Category 
-function edtSc() {
-    $.post("", {
+function edtSc(){
+    $.post("",{
         rolnumber: $('#rolNum').val(),
         categoryname: $('#edtCn').val(),
         onlinestatus: $('#edtOns').val(),
         offlinestatus: $('#edtOfs').val()
-    }, function (data) {
+    }, function(data){
         alert('Edit and Update New Super Category')
     })
 }
-
-
-
-
-
-
-
-
-
-
-
